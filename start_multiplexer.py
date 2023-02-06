bd67ce86f260047a79bc284724123fa0c2c96bf7
Robert Putt <robe8437@C02FV3BTMD6R.local>
2023-02-06 21:08:28 +0000

    Configure TTN

--- start_multiplexer.py
+++ start_multiplexer.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 import os
+import json
 from hm_pyhelper.miner_param import get_ethernet_addresses
 from jinja2 import Template
 
 
 CONF_FILE = '/etc/chirpstack-packet-multiplexer/chirpstack-packet-multiplexer.toml'
+TTN_CONF_FILE = '/var/nebra/ttn_conf.json'
+
+
+def read_ttn_config():
+    try:
+        with open(TTN_CONF_FILE) as file_:
+            ttn_config = json.loads(file_.read())
+
+        return ttn_config
+    except FileNotFoundError:
+        # Config file doesn't exist yet, assume TTN not enabled.
+        return {
+            'ttn_enabled': False,
+            'ttn_cluster': 'eu'
+        }
 
 
 def populate_conf_file():
@@ -19,7 +35,8 @@ def populate_conf_file():
     with open(CONF_FILE) as file_:
         template = Template(file_.read())
 
-    rendered = template.render(gateway_id=gateway_id)
+    ttn_config = read_ttn_config()
+    rendered = template.render(gateway_id=gateway_id, ttn_config=ttn_config)
     with open(CONF_FILE, "w") as file_:
         file_.write(rendered)
 
